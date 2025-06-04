d19a8023ea82e589e17081464f2d788e33f23ad8
ABHCH <chaturvedia435@gmail.com>
2025-06-04 22:25:51 +0530

    adding origin - add verce

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 
 // need to pass option in cors
 const options = {
-  origin: "http://localhost:5173",
+  origin: "https://frontend-murf.vercel.app",
   credentials: true
 }
 app.use(express.json());
